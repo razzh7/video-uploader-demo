80765ff3910840d889dbc472446da8997d283cc2
razzh <razzhavenir@163.com>
2023-03-04 21:06:29 +0800

    chore: 端口修改

--- front/src/util.js
+++ front/src/util.js
@@ -1,8 +1,8 @@
-import axios from "axios";
+import axios from 'axios'
 
 export const http = axios.create({
-    baseURL: 'http://localhost:5000/',
-    headers: {
-        'Content-Type': 'multipart/form-data'
-    }
+  baseURL: 'http://localhost:6003/',
+  headers: {
+    'Content-Type': 'multipart/form-data',
+  },
 })
